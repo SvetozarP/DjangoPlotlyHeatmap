3fbbff8d309e1d81f8897f0a1c341f0da02e0578
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-25 11:18:27 +0100

    Created models Repository and Commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from model_utils.models import TimeStampedModel
+
 
 # Create your models here.
+
+
+class Repository(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start = models.PositiveIntegerField(default=0)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Commit(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='commits')
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    code = models.TextField()
+    hash = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f'commit {self.hash}'
