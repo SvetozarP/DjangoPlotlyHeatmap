eb9a2bfc8b882a008e8bc742449c8cf9d4d5d4ce
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-25 11:23:48 +0100

    Added function to generate hash and passed to commit model.

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from model_utils.models import TimeStampedModel
+from core import utils
 
 
 # Create your models here.
@@ -20,7 +21,7 @@ class Commit(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='commits')
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
     code = models.TextField()
-    hash = models.CharField(max_length=40)
+    hash = models.CharField(max_length=40, default=utils.generate_hash) # Generate hash if one isn't passed
 
     def __str__(self):
         return f'commit {self.hash}'
